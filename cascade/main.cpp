96d39d67eb00e52972ac541bd1e76552396f1271
ArifAbdullaew <fhba246@gmail.com>
2025-04-13 21:51:24 +0300

    added print register for Verilalot

--- cascade/main.cpp
+++ cascade/main.cpp
@@ -1,22 +1,23 @@
 #include "Vbiriscv_tiny_soc.h"
+#include "Vbiriscv_tiny_soc__Syms.h" // важно!
 #include "verilated.h"
 #include <iostream>
+#include <iomanip>
 
 int main(int argc, char **argv) {
     Verilated::commandArgs(argc, argv);
 
     Vbiriscv_tiny_soc* top = new Vbiriscv_tiny_soc;
 
+    // Сброс
     top->clk_i = 0;
     top->rst_i = 1;
-
     for (int i = 0; i < 5; ++i) {
         top->clk_i = !top->clk_i;
         top->eval();
     }
 
     top->rst_i = 0;
-
     for (int i = 0; i < 100; ++i) {
         top->clk_i = !top->clk_i;
         top->eval();
@@ -24,11 +25,13 @@ int main(int argc, char **argv) {
 
     std::cout << "Found a stop request." << std::endl;
 
-    auto& regfile = top->u_core->u_issue->u_regfile;
+    Vbiriscv_tiny_soc__Syms* symsp = top->vlSymsp.get();
+    auto& regfile = symsp->TOP__biriscv_tiny_soc__u_core__u_issue__u_regfile;
 
     for (int i = 1; i <= 31; ++i) {
         uint32_t val = regfile.get_register(i);
-        std::cout << "Dump of reg x" << i << ": 0x" << std::hex << val << std::endl;
+        std::cout << "Dump of reg x" << std::dec << i << ": 0x" 
+                  << std::hex << std::setw(8) << std::setfill('0') << val << std::endl;
     }
 
     delete top;
