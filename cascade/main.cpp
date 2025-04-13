16d14ec9bf3b1ec9bd1b85082ceef24630a530c6
ArifAbdullaew <fhba246@gmail.com>
2025-04-13 21:32:14 +0300

    added print register for Verilalot

--- cascade/main.cpp
+++ cascade/main.cpp
@@ -1,10 +1,11 @@
 #include "Vbiriscv_tiny_soc.h"
+#include "Vbiriscv_tiny_soc__Syms.h"
 #include "verilated.h"
 #include <iostream>
-#include <cstdint>
 
 int main(int argc, char **argv) {
     Verilated::commandArgs(argc, argv);
+
     Vbiriscv_tiny_soc* top = new Vbiriscv_tiny_soc;
 
     top->clk_i = 0;
@@ -22,12 +23,15 @@ int main(int argc, char **argv) {
         top->eval();
     }
 
-    std::cout << "Found a stop request." << std::endl;
-
-    for (int i = 1; i <= 31; ++i) {
-        uint32_t val = top->rootp->TOP__biriscv_tiny_soc__u_core__u_issue__u_regfile.REGFILE__DOT__get_register(i);
-        printf("Dump of reg x%02d: 0x%08x\n", i, val);
+    for (int i = 1; i <= 31; i++) {
+        uint32_t val = top->rootp->TOP__biriscv_tiny_soc__u_core__u_issue__u_regfile.*(
+            &Vbiriscv_tiny_soc_biriscv_regfile::__PVT__REGFILE__DOT__reg_r1_q + (i - 1)
+        );
+        std::cout << "Dump of reg x" << std::dec << i << ": 0x"
+                  << std::hex << val << std::endl;
     }
+
+    std::cout << "Found a stop request." << std::endl;
 
     delete top;
     return 0;
