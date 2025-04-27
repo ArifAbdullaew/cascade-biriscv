64b864e0f2863427852877b364691a7f18ec5a49
ArifAbdullaew <fhba246@gmail.com>
2025-04-28 00:14:05 +0300

    change get method

--- cascade/tb/tb_verilator_simple/main_dump.cpp
+++ cascade/tb/tb_verilator_simple/main_dump.cpp
@@ -1,9 +1,10 @@
 #include "Vbiriscv_tiny_soc.h"
-#include "Vbiriscv_tiny_soc_riscv_core.h"
 #include "Vbiriscv_tiny_soc_biriscv_tiny_soc.h"
-#include "Vbiriscv_tiny_soc___024root.h"
+#include "Vbiriscv_tiny_soc_riscv_core.h"
 #include "Vbiriscv_tiny_soc_biriscv_issue.h"
-#include "Vbiriscv_tiny_soc_biriscv_regfile.h"
+#include "Vbiriscv_tiny_soc_biriscv_regfile.h"   
+#include "Vbiriscv_tiny_soc___024root.h"
+
 #include <verilated.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -40,29 +41,13 @@ int main(int argc, char **argv) {
         top->clk_i = 1;
         top->eval();
 
-        if (top->rootp->biriscv_tiny_soc->u_core->writeback_mem_valid_w) {
-            std::cout << "Cycle: " << cycle 
-                      << " | Memory Access at address: 0x" 
-                      << std::hex 
-                      << (top->rootp->biriscv_tiny_soc->u_core->u_lsu->mem_addr_q) 
-                      << std::dec << std::endl;
-        }
-
         if (cycle % 1000 == 0) {
-            bool printed = false;
+            std::cout << "Cycle: " << cycle << std::endl;
             for (int i = 0; i < 32; i++) {
                 uint32_t reg = top->rootp->biriscv_tiny_soc->u_core->u_issue->u_regfile->REGFILE__DOT__get_register(i);
-                if (reg != 0) {
-                    if (!printed) {
-                        std::cout << "Registers at cycle " << cycle << ":" << std::endl;
-                        printed = true;
-                    }
-                    std::cout << "x" << i << ": 0x" << std::hex << reg << std::dec << std::endl;
-                }
+                std::cout << "[DEBUG] Parsed int reg x" << i << ": " << reg << std::endl;
             }
-            if (printed) {
-                std::cout << "------------------------------" << std::endl;
-            }
+            std::cout << "------------------------------" << std::endl;
         }
     }
 
